0b5c670c142c9e1276538eb5335a2c886cc1d8bb
lauracarlotta <carlotta.custodio@gmail.com>
2022-06-06 18:26:53 -0300

    Fix indentation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js App" />
+	</div>
 </template>
 
 <script>
 // @ is an alias to /src
+// eslint-disable-next-line import/no-unresolved
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+	name: 'HomeView',
+	components: {
+		HelloWorld
+	}
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
+const { defineConfig } = require('@vue/cli-service');
 
-  pages: {
-    index: {
-      entry: 'src/main.js',
-      title: 'Hamburger Store',
-    },
-  },
-})
+module.exports = defineConfig({
+	transpileDependencies: true,
+
+	pages: {
+		index: {
+			entry: 'src/main.js',
+			title: 'Hamburger Store',
+		},
+	},
+});
